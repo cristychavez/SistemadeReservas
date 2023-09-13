de40bd4e6c552c2717dee381f1f7cd4c9340fcc0
cristina <cristychavez974@gmail.com>
2023-09-13 08:59:58 -0600

    creacion de entidades de negocio

---
+++ SistemadeReservas.EntiadadesDeNegocio/Reserva.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponenModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemadeReservas.EntiadadesDeNegocio
+{
+    public class Reserva
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Mesa")]
+        [Required(ErrorMessage = "La mesa es requerida")]
+        [Display(Name = "Mesa")]
+        public int IdMesa { get; set; }
+
+        [Required(ErrorMessage = "El servicio es requerido")]
+        [MaxLength(100, ErrorMessage = "El largo máximo es 100 caracteres")]
+        public string Servicio { get; set; }
+
+        [Required(ErrorMessage = "El cliente es requerido")]
+        [MaxLength(200, ErrorMessage = "El largo máximo es 50 caracteres")]
+        public string Cliente { get; set; }
+
+        [Required(ErrorMessage = "El telefono es requerido")]
+        [MaxLength(200, ErrorMessage = "El largo máximo es 20 caracteres")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "La fecha es requerida")]
+        [MaxLength(200, ErrorMessage = "El largo máximo es 20 caracteres")]
+        public string Fecha { get; set; }
+
+        [Required(ErrorMessage = "La persona es requerida")]
+        [MaxLength(200, ErrorMessage = "El largo máximo es 20 caracteres")]
+        public string Persona { get; set; }
+
+        [Required(ErrorMessage = "La horario entrada es requerida")]
+        [MaxLength(200, ErrorMessage = "El largo máximo es 20 caracteres")]
+        public string HorarioEntrada { get; set; }
+
+        [Required(ErrorMessage = "La horario salida es requerida")]
+        [MaxLength(200, ErrorMessage = "El largo máximo es 20 caracteres")]
+        public string HorarioSalida { get; set; }
+
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+        public Departamento Departamento { get; set; }
+
+
+    }
+}
---
+++ SistemadeReservas.EntiadadesDeNegocio/Usuario.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemadeReservas.EntiadadesDeNegocio
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Rol")]
+        [Required(ErrorMessage = "El Rol es obligatorio")]
+        [Display(Name = "Rol")]
+        public int RolId { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Máximo 30 caracteres")]
+        public string Nombre { get; set; }
+
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [StringLength(30, ErrorMessage = "Máximo 30 caracteres")]
+        public string Apellido { get; set; }
+
+
+        [Required(ErrorMessage = "El Login es obligatorio")]
+        [StringLength(25, ErrorMessage = "Máximo 25 caracteres")]
+        public string Login { get; set; }
+
+
+        [Required(ErrorMessage = "El Password es obligatorio")]
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "Password debe estar entre 6 a 50 caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "El estatus es obligatorio")]
+        public byte Estatus { get; set; }
+
+
+        [Display(Name = "Fecha registro")]
+        public DateTime FechaRegistro { get; set; }
+
+        //propiedad de navegación
+        public Rol Rol { get; set; }
+
+        //propiedades auxiliares
+        [NotMapped]
+        public int top_aux { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "Confirmar el password")]
+        [StringLength(50, ErrorMessage = "Password debe estar entre 6 a 50 caracteres", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password y confirmar password deben de ser iguales")]
+        [Display(Name = "Confirmar password")]
+        public string confirmPassword_aux { get; set; }
+    }
+
+    public enum Estatus_Usuario
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
+    }
+}
